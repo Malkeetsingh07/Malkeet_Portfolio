71706276f962d21729839ab56ab73af1ad465238
unknown <malkeetsingh01791@gmail.com>
2026-05-29 20:38:50 +0530

    Updated portfolio website

--- script.js
+++ script.js
@@ -1,20 +1,7 @@
-<<<<<<< HEAD
-// Contact Form Alert
-
 document.getElementById("contactForm").addEventListener("submit", function(e){
 
   e.preventDefault();
 
   alert("Thank You! Your message has been sent.");
 
-=======
-// Contact Form Alert
-
-document.getElementById("contactForm").addEventListener("submit", function(e){
-
-  e.preventDefault();
-
-  alert("Thank You! Your message has been sent.");
-
->>>>>>> 548d05101591c94a04b817bd07f9c010d9088434
 });
